3f73c24378da13a29bf92b51e8bd9de43e74a5fc
坂口雄亮 <gupisakaguchi@gmail.com>
2017-01-06 05:44:27 +0900

    add exam

--- exam05.rb
+++ exam05.rb
@@ -18,3 +18,26 @@ class C1
 end
 c2 = C1::C2.new
 c2.a
+
+#module_function
+module M
+  def call_a
+    a
+  end
+  
+  module_function
+  def a
+    puts "a"
+  end
+end
+M.a
+
+class C2
+  include M
+  def foo
+    M.a
+  end
+end
+
+c2 = C2.new
+c2.call_a
