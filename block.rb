54d6d00186b4f998d14ab8580447f01865ac0ae8
坂口雄亮 <gupisakaguchi@gmail.com>
2017-01-07 00:05:07 +0900

    block

--- block.rb
+++ block.rb
@@ -12,3 +12,29 @@ p func(1,&pro)
 
 lmd = ->(x) {3}
 #lmd.call
+
+#脱出
+def foo
+  f = Proc.new { return :foo }
+  f.call
+  return
+end
+
+def bar
+  f = lambda { return :bar }
+  f.call
+  return
+end
+
+def h
+  yield
+end
+
+def hoge
+  h{ return :hoge }
+  nil
+end
+
+p foo()  #=> :foo
+p bar()  #=> nil
+p hoge() #=> :hoge
